5087dafcb4a2e197047146d93631e458e893dd8d
IamNalim <48150037+IamNalim@users.noreply.github.com>
2020-07-22 16:13:01 +0200

    Update stopwatch.py
    Some fixes for PEP8

--- stopwatch.py
+++ stopwatch.py
@@ -1,20 +1,20 @@
 from time import sleep
 
-#variables for work
+# variables for work
 sec = minute = hour = 0
 
-#main loop for stopwatch
+# main loop for stopwatch
 while True:
     sleep(1)
     sec += 1
-    #zjsitime zda ubehlo 60 sekund
+    # zjsitime zda ubehlo 60 sekund
     if sec / 60 >= 1:
         minute += 1
         sec -= 60
-        #zjistime zda ubehlo 60 minut
-        if minute / 60 >=1:
+        # zjistime zda ubehlo 60 minut
+        if minute / 60 >= 1:
             hour += 1
             minute -= 60
-            #todo
-    
+            # todo
+
     print('Time: {} hodin, {} minut, {} sekund'.format(hour, minute, sec))
